6b52de399de3cd669bfbaafeb78e4b33036d77b8
Elias Silva <pro.eliasdns@gmail.com>
2024-06-19 17:33:26 -0300

    add utils.powerbi

---
+++ utils/powerbi.py
@@ -0,0 +1,37 @@
+import csv
+from pathlib import Path
+import requests
+from django.conf import settings
+
+
+# example_payload = {  # test-mostra-stream
+#     'nome': 'Elias',
+#     'votos1': 10,
+#     'votos2': 98.6,
+#     'votos3': 150,
+#     'votos4': 98.6,
+#     'votos5': 105,
+# }
+example_payload = [{  # mostra-stream
+    'total_votos': 21,
+    'total_genero_masculino': 10,
+    'total_genero_feminino': 6,
+    'preferred_dispositivo': 'Smartphone',
+    'preferred_cor': 'Blue',
+    'media_idade': 23.6,
+    'media_opiniao': 3.5,
+
+    'grafico': 'Teste',
+    'min_media_opiniao': 1,
+    'max_media_opiniao': 5,
+}]
+
+
+def push_stream_api(payload: dict, endpoint: str = settings.POWERBI_SERVICE_STREAM_ENDPOINT):
+    return requests.post(endpoint, json=payload, timeout=5)
+
+
+def push_csv(payload: dict, csv_path: str | Path = settings.CSV_STREAM_PATH):
+    with open(csv_path, 'a+', encoding='utf-8', newline='\n') as csv_file:  # newline='\r\n'
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(payload.values())

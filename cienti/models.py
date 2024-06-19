f6b97933a0c5e5999c61cd84755130308c9e5d04
Elias Silva <pro.eliasdns@gmail.com>
2024-06-19 18:34:01 -0300

    models: add Voto.objects.aggs_as_payload

--- cienti/models.py
+++ cienti/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Q, Count, Avg
 
 
 class Voto(models.Model):
@@ -6,9 +7,6 @@ class Voto(models.Model):
         MASCULINO = 'M', 'Masculino'
         FEMININO = 'F', 'Feminino'
         OUTRO = 'O', 'Outro/Prefiro não dizer'
-    genero = models.CharField(max_length=1, choices=Genero.choices, verbose_name='gênero')
-
-    idade = models.PositiveSmallIntegerField()
 
     class Dispositivo(models.TextChoices):
         DESKTOP = 'Desktop'
@@ -16,7 +14,6 @@ class Voto(models.Model):
         TABLET = 'Tablet'
         SMARTPHONE = 'Smartphone'
         OUTRO = 'Outro'
-    dispositivo = models.CharField(max_length=10, choices=Dispositivo.choices)
 
     class Opiniao(models.IntegerChoices):
         MUITO_FAVORAVEL = 1, 'Muito Favorável'
@@ -24,7 +21,6 @@ class Voto(models.Model):
         NEUTRA = 3, 'Neutra'
         DESFAVORAVEL = 4, 'Desfavorável'
         MUITO_DESFAVORAVEL = 5, 'Muito Desfavorável'
-    opiniao = models.PositiveSmallIntegerField(choices=Opiniao.choices)
 
     class Cor(models.TextChoices):
         RED = 'Red'
@@ -35,11 +31,35 @@ class Voto(models.Model):
         YELLOW = 'Yellow'
         BLACK = 'Black'
         WHITE = 'White'
+
+    class VotoManager(models.Manager):
+        def aggs_as_payload(self):
+            aggregates = self.aggregate(
+                total_votos=Count('id'),
+                total_genero_masculino=Count('id', filter=Q(genero=Voto.Genero.MASCULINO)),
+                total_genero_feminino=Count('id', filter=Q(genero=Voto.Genero.FEMININO)),
+                media_idade=Avg('idade'),
+                media_opiniao=Avg('opiniao'),
+            )
+
+            preferred_dispositivo = self.values('dispositivo').annotate(count=Count('dispositivo')).order_by('-count').first()
+            preferred_cor = self.values('cor').annotate(count=Count('cor')).order_by('-count').first()
+
+            aggregates['preferred_dispositivo'] = preferred_dispositivo['dispositivo'] if preferred_dispositivo else None
+            aggregates['preferred_cor'] = preferred_cor['cor'] if preferred_cor else None
+
+            return aggregates
+    objects = VotoManager()
+
+    genero = models.CharField(max_length=1, choices=Genero.choices, verbose_name='gênero')
+    idade = models.PositiveSmallIntegerField()
+    dispositivo = models.CharField(max_length=10, choices=Dispositivo.choices)
+    opiniao = models.PositiveSmallIntegerField(choices=Opiniao.choices)
     cor = models.CharField(max_length=10, choices=Cor.choices)
 
     def __str__(self) -> str:
         return f'#{self.pk}'
 
     @property
-    def as_payload(self):
+    def obj_as_payload(self):
         return {f.name: getattr(self, f.name) for f in Voto._meta.fields}  # if f.name != 'id'
